db7be8fce64989dc3af74911a946f3f73643f686
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 13:36:44 -0600

    Update MtMoon1F.asm

--- data/wild/maps/MtMoon1F.asm
+++ data/wild/maps/MtMoon1F.asm
@@ -1,15 +1,15 @@
 MtMoon1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 	db  8, ZUBAT
-	db  7, ZUBAT
+	db  7, GEODUDE
 	db  9, ZUBAT
-	db  8, GEODUDE
-	db  6, ZUBAT
+	db  9, GEODUDE
+	db  8, PARAS
 	db 10, ZUBAT
 	db 10, GEODUDE
-	db  8, PARAS
+	db  9, PARAS
 	db 11, ZUBAT
-	db  8, CLEFAIRY
+	db 11, CLEFAIRY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
