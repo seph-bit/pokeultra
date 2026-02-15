f68c68aacb98befa0ca2e1d3d7b4cdee06e993b9
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 14:43:02 -0600

    Update Route5.asm

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,29 +1,26 @@
 Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 13, ODDISH
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MANKEY
-	db 12, MANKEY
-	db 15, ODDISH
+	db 15, MANKEY
 	db 16, ODDISH
-	db 16, PIDGEY
-	db 14, MANKEY
 	db 16, MANKEY
+	db 17, GROWLITHE
+	db 19, ODDISH
+	db 17, MEOWTH
+	db 18, MEOWTH
 ENDC
 IF DEF(_BLUE)
-	db 13, BELLSPROUT
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MEOWTH
-	db 12, MEOWTH
-	db 15, BELLSPROUT
+	db 15, MEOWTH
 	db 16, BELLSPROUT
-	db 16, PIDGEY
-	db 14, MEOWTH
 	db 16, MEOWTH
+	db 17, VULPIX
+	db 19, BELLSPROUT
+	db 17, MEOWTH
+	db 18, MANKEY
 ENDC
+	db 16, JIGGLYPUFF
+	db 18, JIGGLYPUFF
+	db 20, WIGGLYTUFF
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
