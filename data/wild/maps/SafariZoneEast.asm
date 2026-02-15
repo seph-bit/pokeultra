d9e93b033c170d14d3221d1c9aee04372179a6f8
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 17:29:53 -0600

    Update SafariZoneEast.asm

--- data/wild/maps/SafariZoneEast.asm
+++ data/wild/maps/SafariZoneEast.asm
@@ -1,28 +1,28 @@
 SafariZoneEastWildMons:
 	def_grass_wildmons 30 ; encounter rate
 IF DEF(_RED)
-	db 24, NIDORAN_M
-	db 26, DODUO
-	db 22, PARAS
-	db 25, EXEGGCUTE
-	db 33, NIDORINO
-	db 23, EXEGGCUTE
-	db 24, NIDORAN_F
-	db 25, PARASECT
-	db 25, KANGASKHAN
-	db 28, SCYTHER
+	db 31, GLOOM
+	db 30, EXEGGCUTE
+	db 30, PIKACHU
+	db 32, PARASECT
+	db 33, WEEPINBELL
+	db 32, EXEGGCUTE
+	db 33, PIKACHU
+	db 34, PARASECT
+	db 35, PINSIR
+	db 36, SCYTHER
 ENDC
 IF DEF(_BLUE)
-	db 24, NIDORAN_F
-	db 26, DODUO
-	db 22, PARAS
-	db 25, EXEGGCUTE
-	db 33, NIDORINA
-	db 23, EXEGGCUTE
-	db 24, NIDORAN_M
-	db 25, PARASECT
-	db 25, KANGASKHAN
-	db 28, PINSIR
+	db 31, WEEPINBELL
+	db 30, EXEGGCUTE
+	db 30, PIKACHU
+	db 32, PARASECT
+	db 33, GLOOM
+	db 32, EXEGGCUTE
+	db 33, PIKACHU
+	db 34, PARASECT
+	db 35, SCYTGER
+	db 36, PINSIR
 ENDC
 	end_grass_wildmons
 
