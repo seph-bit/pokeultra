536f3b3035757b23c758b1ae57174723c8624c7c
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 17:27:14 -0600

    Update SafariZoneCenter.asm

--- data/wild/maps/SafariZoneCenter.asm
+++ data/wild/maps/SafariZoneCenter.asm
@@ -2,10 +2,10 @@ SafariZoneCenterWildMons:
 	def_grass_wildmons 30 ; encounter rate
 IF DEF(_RED)
 	db 31, PSYDUCK
-	db 30, RHYHORN
+	db 30, KINGLER
 	db 33, SANDSLASH
 	db 33, SLOWPOKE
-	db 32, RHYHORN
+	db 32, SHELLDER
 	db 33, GOLDUCK
 	db 34, SHELLDER
 	db 35, SANDSLASH
@@ -14,10 +14,10 @@ IF DEF(_RED)
 ENDC
 IF DEF(_BLUE)
 	db 31, SLOWWPOKE
-	db 30, RHYHORN
+	db 30, KINGLER
 	db 33, SANDSLASH
 	db 33, PSYDUCK
-	db 32, RHYHORN
+	db 32, STARYU
 	db 33, SLOWBRO
 	db 34, STARYU
 	db 35, SANDSLASH
