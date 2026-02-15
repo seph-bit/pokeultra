e1f4cfad9924e0321a4ee8f91baae994170a6b76
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 14:59:39 -0600

    Update Route11.asm

--- data/wild/maps/Route11.asm
+++ data/wild/maps/Route11.asm
@@ -1,26 +1,26 @@
 Route11WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 14, EKANS
-	db 15, SPEAROW
-	db 12, EKANS
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
 	db 15, EKANS
+	db 17, DROWZEE
+	db 18, EKANS
+	db 18, SLOWPOKE
+	db 16, SPEAROW
+	db 20, DROWZEE
+	db 18, SANDSHREW
 ENDC
 IF DEF(_BLUE)
-	db 14, SANDSHREW
-	db 15, SPEAROW
-	db 12, SANDSHREW
-	db  9, DROWZEE
-	db 13, SPEAROW
-	db 13, DROWZEE
 	db 15, SANDSHREW
+	db 17, DROWZEE
+	db 18, SANDSHREW
+	db 18, SLOWPOKE
+	db 16, SPEAROW
+	db 20, DROWZEE
+	db 18, EKANS
 ENDC
-	db 17, SPEAROW
-	db 11, DROWZEE
-	db 15, DROWZEE
+	db 19, SPEAROW
+	db 18, LICKITUNG
+	db 21, LICKITUNG
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
