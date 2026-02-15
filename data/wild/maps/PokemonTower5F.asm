24cfb4d5ff1b02f119a68b0cf13774428260cdd0
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 16:40:20 -0600

    Update PokemonTower5F.asm

--- data/wild/maps/PokemonTower5F.asm
+++ data/wild/maps/PokemonTower5F.asm
@@ -1,15 +1,15 @@
 PokemonTower5FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 20, GASTLY
-	db 21, GASTLY
-	db 22, GASTLY
+	db 25, GASTLY
+	db 21, PARASECT
+	db 22, RATICATE
 	db 23, GASTLY
-	db 19, GASTLY
-	db 18, GASTLY
+	db 26, GASTLY
+	db 27, GASTLY
 	db 25, HAUNTER
-	db 20, CUBONE
-	db 22, CUBONE
-	db 24, GASTLY
+	db 25, HAUNTER
+	db 26, PARASECT
+	db 28, HAUNTER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
