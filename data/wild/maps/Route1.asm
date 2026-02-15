69f8cde4c1a41fa5d13dc22e469529f5e6a0ac21
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 13:09:42 -0600

    Update Route1.asm

--- data/wild/maps/Route1.asm
+++ data/wild/maps/Route1.asm
@@ -2,14 +2,14 @@ Route1WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  3, PIDGEY
 	db  3, RATTATA
-	db  3, RATTATA
-	db  2, RATTATA
-	db  2, PIDGEY
-	db  3, PIDGEY
-	db  3, PIDGEY
+	db  3, CATERPIE
 	db  4, RATTATA
 	db  4, PIDGEY
 	db  5, PIDGEY
+	db  3, CATERPIE
+	db  5, RATTATA
+	db  4, PIKACHU
+	db  5, PIKACHU
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
