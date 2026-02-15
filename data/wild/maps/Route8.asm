4fdc7bb208e8d8e331a8c019c89565554a3d0138
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 16:50:23 -0600

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -3,26 +3,24 @@ Route8WildMons:
 	db 24, PIDGEOTTO
 IF DEF(_RED)
 	db 25, GROWLITHE
-	db 28, PRIMEAPE
+	db 28, KADABRA
 	db 26, GROWLITHE
 	db 27, PIDGEOTTO
 	db 25, VULPIX
 	db 24, KADABRA
 	db 26, VULPIX
-	db 26, KADABRA
-	db 27, KADABRA
 ENDC
 IF DEF(_BLUE)
 	db 25, VULPIX
-	db 28, PERSIAN
+	db 28, KADABRA
 	db 26, VULPIX
 	db 27, PIDGEOTTO
 	db 25, GROWLITHE
 	db 24, KADABRA
 	db 26, GROWLITHE
-	db 26, KADABRA
-	db 27, KADABRA
 ENDC
+	db 26, MR_MIME
+	db 27, MR_MIME
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
