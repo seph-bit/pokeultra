d6eb8b4cecda2ac38eba818b265ba40afb97681c
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 14:52:14 -0600

    Update Route6.asm

--- data/wild/maps/Route6.asm
+++ data/wild/maps/Route6.asm
@@ -1,29 +1,27 @@
 Route6WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 13, ODDISH
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MANKEY
-	db 12, MANKEY
-	db 15, ODDISH
-	db 16, ODDISH
-	db 16, PIDGEY
-	db 14, MANKEY
-	db 16, MANKEY
+	db 15, MAGNEMITE
+	db 15, MANKEY
+	db 16, PSYDUCK
+	db 17, MANKEY
+	db 20, PIDGEOTTO
+	db 17, MAGNEMITE
+	db 19, MAGNEMITE
 ENDC
 IF DEF(_BLUE)
-	db 13, BELLSPROUT
-	db 13, PIDGEY
-	db 15, PIDGEY
-	db 10, MEOWTH
-	db 12, MEOWTH
-	db 15, BELLSPROUT
-	db 16, BELLSPROUT
-	db 16, PIDGEY
-	db 14, MEOWTH
-	db 16, MEOWTH
+	db 15, MAGNEMITE
+	db 15, MEOWTH
+	db 16, PSYDUCK
+	db 17, MEOWTH
+	db 20, PIDGEOTTO
+	db 17, MAGNEMITE
+	db 19, MAGNEMITE
+	db 19, PSYDUCK
 ENDC
+	db 19, PSYDUCK
+	db 21, LICKITUNG
+	db  5, SQUIRTLE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
