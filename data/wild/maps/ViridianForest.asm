4508d937be29fd1fe15f693ac0ee3803025d9f7f
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 13:24:46 -0600

    Update ViridianForest.asm

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -2,26 +2,25 @@ ViridianForestWildMons:
 	def_grass_wildmons 8 ; encounter rate
 IF DEF(_RED)
 	db  4, WEEDLE
-	db  5, KAKUNA
-	db  3, WEEDLE
-	db  5, WEEDLE
-	db  4, KAKUNA
-	db  6, KAKUNA
-	db  4, METAPOD
-	db  3, CATERPIE
+	db  4, PIDGEY
+	db  5, POLIWAG
+	db  5, CATERPIE
+	db  5, PIDGEY
+	db  6, WEEDLE
+	db  6, POLIWAG
 ENDC
 IF DEF(_BLUE)
-	db  4, CATERPIE
-	db  5, METAPOD
-	db  3, CATERPIE
+	db  4, WEEDLE
+	db  4, PIDGEY
+	db  5, POLIWAG
 	db  5, CATERPIE
-	db  4, METAPOD
-	db  6, METAPOD
-	db  4, KAKUNA
-	db  3, WEEDLE
+	db  5, PIDGEY
+	db  6, WEEDLE
+	db  6, POLIWAG
 ENDC
-	db  3, PIKACHU
-	db  5, PIKACHU
+    db  5, PIKACHU
+	db  7, PIKACHU
+	db  5, BULBASAUR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
