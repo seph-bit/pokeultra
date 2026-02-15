d1afbfda13c6a9ec78a0359afae4d8b0dca26924
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 17:05:38 -0600

    Update Route18.asm

--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -1,15 +1,15 @@
 Route18WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
-	db 25, FEAROW
-	db 26, DODUO
-	db 28, DODUO
-	db 29, RATICATE
-	db 27, FEAROW
+	db 29, GOLDUCK
 	db 29, FEAROW
+	db 31, DODRIO
+	db 32, PARASECT
+	db 32, FEAROW
+	db 33, SLOWBRO
+	db 32, DODRIO
+	db 33, PARASECT
+	db 31, TAUROS
+	db 34, TAUROS
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
