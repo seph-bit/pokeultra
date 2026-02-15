70a1275317874264ff7ed9b4966d09bf07cd9d4f
seph-bit <seraphim.beatle@gmail.com>
2026-02-15 16:42:02 -0600

    Update PokemonTower6F.asm

--- data/wild/maps/PokemonTower6F.asm
+++ data/wild/maps/PokemonTower6F.asm
@@ -1,15 +1,15 @@
 PokemonTower6FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
-	db 20, GASTLY
-	db 19, GASTLY
-	db 26, HAUNTER
-	db 22, CUBONE
-	db 24, CUBONE
-	db 28, HAUNTER
+	db 27, GASTLY
+	db 25, HAUNTER
+	db 29, VULPIX
+	db 26, CUBONE
+	db 27, GASTLY
+	db 28, GASTLY
+	db 27, HAUNTER
+	db 28, CUBONE
+	db 26, MAROWAK
+	db 29, GENGAR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
