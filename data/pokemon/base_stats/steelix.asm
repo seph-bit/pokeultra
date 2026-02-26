aed27db74aa8fdb1a9b6443b2d932db784d652e4
seph-bit <seraphim.beatle@gmail.com>
2026-02-26 08:51:26 -0600

    Create steelix.asm

---
+++ data/pokemon/base_stats/steelix.asm
@@ -0,0 +1,23 @@
+	db DEX_ONIX ; pokedex id
+
+	db  75,  85, 200,  30,  55
+	;   hp  atk  def  spd  spc
+
+	db ROCK, GROUND ; type
+	db 45 ; catch rate
+	db 108 ; base exp
+
+	INCBIN "gfx/pokemon/front/onix.pic", 0, 1 ; sprite dimensions
+	dw SteelixPicFront, SteelixPicBack
+
+	db TACKLE, SCREECH, BIND, ROCK_THROW ; level 1 learnset
+	db GROWTH_MEDIUM_FAST ; growth rate
+
+	; tm/hm learnset
+	tmhm TOXIC,        BODY_SLAM,    TAKE_DOWN,    DOUBLE_EDGE,  RAGE,         \
+	     EARTHQUAKE,   FISSURE,      DIG,          MIMIC,        DOUBLE_TEAM,  \
+	     BIDE,         SELFDESTRUCT, SKULL_BASH,   REST,         EXPLOSION,    \
+	     ROCK_SLIDE,   SUBSTITUTE,   STRENGTH
+	; end
+
+	db 0 ; padding
